6c885823608e00aa9a40cc8739ab43e10668d67c
Bruno Cabral <cabral.brc@gmail.com>
2021-10-02 09:50:53 -0700

    p1 input

--- connect4.py
+++ connect4.py
@@ -7,4 +7,17 @@ def create_board():
     return board
 
 board = create_board()
+game_over = False # True = someone wins (4 in a row)
+turn = 0 # 0 - p1 turn
+
+while not game_over:
+    # Ask for Player 1 input
+    if turn == 0:
+        selection = int(input("Player 1 make your selection (0-6):"))
+        print(selection)
+        print(type(selection))
+        break
+
+    # Ask for Player 2 input
+
 # %%
