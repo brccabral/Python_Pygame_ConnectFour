360e32da183af4b146d394293e4d2f6a2abc3d20
Bruno Cabral <cabral.brc@gmail.com>
2021-10-02 10:00:06 -0700

    check if input col is available

--- connect4.py
+++ connect4.py
@@ -9,8 +9,8 @@ def create_board():
 def drop_piece():
     pass
 
-def is_valid_location():
-    pass
+def is_valid_location(board, col):
+    return board[5][col] == 0
 
 def get_next_open_row():
     pass
@@ -23,12 +23,12 @@ turn = 0 # 0 - p1 turn
 while not game_over:
     # Ask for Player 1 input
     if turn == 0:
-        selection = int(input("Player 1 make your selection (0-6):"))
+        col = int(input("Player 1 make your selection (0-6):"))
         
 
     # Ask for Player 2 input
     else:
-        selection = int(input("Player 2 make your selection (0-6):"))
+        col = int(input("Player 2 make your selection (0-6):"))
 
     turn += 1
     turn = turn % 2
