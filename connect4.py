ad0ccf32084d012807e6393dccafc32e172b62b6
Bruno Cabral <cabral.brc@gmail.com>
2021-10-02 10:06:12 -0700

    p1 and p2 can put piece

--- connect4.py
+++ connect4.py
@@ -10,10 +10,10 @@ def create_board():
     return board
 
 def drop_piece(board, row, col, piece):
-    board[row][col] == piece
+    board[row][col] = piece
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
@@ -30,13 +30,21 @@ while not game_over:
     if turn == 0:
         col = int(input("Player 1 make your selection (0-6):"))
         
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
 
     # Ask for Player 2 input
     else:
         col = int(input("Player 2 make your selection (0-6):"))
 
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+
     turn += 1
     turn = turn % 2
+    print(board)
 
 
 # %%
